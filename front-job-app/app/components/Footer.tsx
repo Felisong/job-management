4ee29e4c2ba3e1a8fa71de8393e1a22cb98bdc60
carolina <Carolinahs100@gmail.com>
2025-10-16 16:46:55 -0600

    made header and footer, got material UI installed for use later.

---
+++ front-job-app/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-amber-300 w-full p-4 items-center">
+      <p className="text-black">copyright @ Carolina 2025</p>
+    </footer>
+  );
+}
---
+++ front-job-app/app/components/Header.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Header() {
+  return (
+    <header className="w-full bg-amber-300 flex justify-between p-4 items-center">
+      <h1 className="text-black">Job App</h1>
+      <div className="w-[50px] h-[50px] bg-red-600 rounded-full"></div>
+    </header>
+  );
+}
