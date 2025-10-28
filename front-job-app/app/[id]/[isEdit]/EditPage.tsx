be3f1c61d49e95d917f7178597966d76c973bbc4
carolina <Carolinahs100@gmail.com>
2025-10-28 15:44:17 -0600

    got more inputs into editing and got docker to just display changes as i code.

--- front-job-app/app/[id]/[isEdit]/EditPage.tsx
+++ front-job-app/app/[id]/[isEdit]/EditPage.tsx
@@ -5,7 +5,10 @@ import { JobInformationModel } from "../../../types";
 export default function EditPage({ job }: { job: JobInformationModel }) {
   return (
     <form action="POST">
+      <TextInputComponent label="Company Name" />
       <TextInputComponent label="Job Title" />
+      <TextInputComponent label="Job Description" />
+      <TextInputComponent label="Other Notes" />
     </form>
   );
 }
