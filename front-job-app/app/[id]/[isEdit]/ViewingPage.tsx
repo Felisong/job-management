b32b3b587eebd0f1291757a879d8dc6c78d59117
carolina <Carolinahs100@gmail.com>
2025-11-04 13:36:29 -0700

    got the view page done and the edit page skeleton built.

--- front-job-app/app/[id]/[isEdit]/ViewingPage.tsx
+++ front-job-app/app/[id]/[isEdit]/ViewingPage.tsx
@@ -3,9 +3,38 @@
 import { JobInformationModel } from "@/types";
 
 export default function ViewingPage({ job }: { job: JobInformationModel }) {
+  function attributeExists(field: keyof JobInformationModel) {
+    return job[field] !== "";
+  }
+  console.log(`job desc: `, job.job_description);
   return (
-    <div>
-      <p>Meow</p>
+    <div className="box-border my-10">
+      <h2>Job Title</h2>
+      <p>{job.job_title}</p>
+      {attributeExists("state") && (
+        <>
+          <h2>Job Status</h2>
+          <p>{job.state}</p>
+        </>
+      )}
+      {attributeExists("date_sent") && (
+        <>
+          <h2>Application Date Sent</h2>
+          <p>{job.date_sent?.substring(0, job.date_sent?.indexOf("T"))}</p>
+        </>
+      )}
+      {attributeExists("job_description") && (
+        <>
+          <h2>Job Description</h2>
+          <p className="whitespace-pre-wrap">{job.job_description}</p>
+        </>
+      )}
+      {attributeExists("other") && (
+        <>
+          <h2>Job Other</h2>
+          <p className="whitespace-pre-wrap">{job.other}</p>
+        </>
+      )}
     </div>
   );
 }
