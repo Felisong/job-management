f49f526d38cc205a7a829b22ed7f5e815de94a2f
Felisong <carolinahs100@gmail.com>
2025-11-28 15:56:44 -0700

    made document schema.

---
+++ back-job-app/app/models/DocumentSchema.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const DocumentSchema = new mongoose.Mongoose.Schema(
+    {
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ['resume', 'coverLetter'],
+        required: true
+    },
+    file_name: {type: String, required: true},
+    size: {type: Number, required: true},
+    filePath: {type: String, required: true},
+    uploaded_at: {
+        type:Date,
+        default: Date.now
+    }
+},
+{collection: 'documents'}
+)
+
+const Documents = mongoose.model('documents', DocumentSchema);
+module.exports = Documents;
--- back-job-app/app/models/Users.js
+++ back-job-app/app/models/Users.js
@@ -5,13 +5,11 @@ const UserSchema = new mongoose.Schema(
     user_name: { type: String, required: true },
     user_email: { type: String, required: true },
     password: { type: String, required: true },
-    resume_path: { type: String, required: true },
-    cover_path: { type: String, required: false },
     other: { type: String, required: false },
   },
-  { collection: "job_information" }
+  { collection: "users" }
 );
 
-const Users = mongoose.model("users", UserSchema);
+const User = mongoose.model("users", UserSchema);
 
-module.exports = Users;
+module.exports = User;
