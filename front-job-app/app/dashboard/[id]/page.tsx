40db4929a9aacde40b1835f035bc2e66fd3a6eba
Felisong <carolinahs100@gmail.com>
2026-01-05 16:14:44 -0700

    got the modal pop up logic in and state change of inputs.

--- front-job-app/app/dashboard/[id]/page.tsx
+++ front-job-app/app/dashboard/[id]/page.tsx
@@ -2,11 +2,13 @@
 
 import { ChangeUserPassword } from "@/app/actions/ChangePassword";
 import { SendValidationEmail } from "@/app/actions/SendValidationEmail";
+import BasicButtonComponent from "@/app/components/BasicButtonComponent";
+import TextInputComponent from "@/app/components/TextInputComponent";
 import { useToast } from "@/app/utils/context/ShowToastContext";
 import { useUser } from "@/app/utils/context/UserDataContext";
 import { getAuthToken } from "@/app/utils/cookies";
 import { useRouter } from "next/navigation";
-import React, { MouseEvent, useEffect, useState } from "react";
+import React, { ChangeEvent, MouseEvent, useEffect, useState } from "react";
 
 export default function UserDashBoard({
   params,
@@ -17,11 +19,31 @@ export default function UserDashBoard({
   const toast = useToast();
   const user = useUser();
   const router = useRouter();
-  const [confirmationInputs, setConfirmationInputs] = useState<{
-    email: string;
-    password: string;
-  }>({ email: "", password: "" });
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [isEmailChange, setisEmailChange] = useState<boolean>(false);
+  const [confirmInput, setConfirmInput] = useState<{
+    email: { label: string; value: string };
+    password: { label: string; value: string };
+  }>({ email: { label: "", value: "" }, password: { label: "", value: "" } });
 
+  // sets visibility of confirmation pop up
+  async function triggerModal(clickedEmailChange: boolean) {
+    setisEmailChange(clickedEmailChange);
+    setShowModal((prev) => !prev);
+  }
+  // handles the state of the input.
+  function handleInputChange(
+    e: React.ChangeEvent<HTMLInputElement>,
+    input: keyof typeof confirmInput
+  ) {
+    setConfirmInput((prev) => ({
+      ...prev,
+      [input]: {
+        label: prev[input].label,
+        value: e.target.value,
+      },
+    }));
+  }
   // call to send an email to the user.
   async function ValidateUser(e: MouseEvent) {
     e.preventDefault();
@@ -43,21 +65,21 @@ export default function UserDashBoard({
       });
     }
   }
-  // trigger Modal
-  async function triggerModal(e: MouseEvent){
-    e.preventDefault();
-    // change variable to opposite of itself.
-  }
+
   // call to change user password
-  async function changePassword(e: MouseEvent) {
-    e.preventDefault();
+  async function changePassword() {
     try {
-      if (confirmationInputs.email !== user.userData.user_email) {
+      triggerModal(true);
+      if (confirmInput.email.value !== user.userData.user_email) {
         throw new Error(
           "email entered does not match our records... Please contact me directly."
         );
-      };
-      await ChangeUserPassword(user.userData.user_id, user.userData.user_email, getAuthToken());
+      }
+      await ChangeUserPassword(
+        user.userData.user_id,
+        user.userData.user_email,
+        getAuthToken()
+      );
       toast.triggerToast({
         message: "Sent email to reset password",
         isError: false,
@@ -72,8 +94,7 @@ export default function UserDashBoard({
     }
   }
   // call to change user email
-  async function ChangeEmail(e: MouseEvent) {
-    e.preventDefault();
+  async function changeEmail() {
     try {
       // fetch
       console.log(`email change triggered`);
@@ -114,46 +135,79 @@ export default function UserDashBoard({
   }, [user.userData, user.initialized, paramId]);
 
   return (
-    <div className="p-4">
-      <h1>User Dashboard</h1>
-      <div className="p-4 flex flex-col gap-8">
-        {!user.userData.validated && (
+    <>
+      {showModal && (
+        <div className="bg-blue-900 p-4">
+          <TextInputComponent
+            label="Email"
+            value={confirmInput.email.value}
+            onChange={(e) => {
+              handleInputChange(e, "email");
+            }}
+          />
+          {isEmailChange && (
+            <TextInputComponent
+              label="Password"
+              value={confirmInput.password.value}
+              onChange={(e) => {
+                handleInputChange(e, "password");
+              }}
+            />
+          )}
+          <BasicButtonComponent
+            label="Submit"
+            action={(e) => {
+              e.preventDefault();
+              if (isEmailChange) {
+                changeEmail();
+              } else {
+                changePassword();
+              }
+            }}
+          />
+        </div>
+      )}
+      <div className="p-4">
+        <h1>User Dashboard</h1>
+        <div className="p-4 flex flex-col gap-8">
+          {!user.userData.validated && (
+            <button
+              className="text-[clamp(1.5rem,_1.7rem,_2rem)] text-start"
+              disabled={!user.initialized}
+              onClick={ValidateUser}
+            >
+              Validate Account{" "}
+            </button>
+          )}
           <button
             className="text-[clamp(1.5rem,_1.7rem,_2rem)] text-start"
             disabled={!user.initialized}
-            onClick={changePassword}
+            onClick={(e) => {
+              e.preventDefault();
+              // mark to show input
+            }}
           >
-            Validate Account{" "}
+            Change Password
           </button>
-        )}
-        <button
-          className="text-[clamp(1.5rem,_1.7rem,_2rem)] text-start"
-          disabled={!user.initialized}
-          onClick={(e) => {
-            e.preventDefault();
-            // mark to show input
-          }}
-        >
-          Change Password
-        </button>
-        <button
-          className="text-[clamp(1.5rem,_1.7rem,_2rem)] text-start"
-          disabled={!user.initialized}
-          onClick={(e) => {
-            e.preventDefault();
-            // mark to show inputs
-          }}
-        >
-          Change Email
-        </button>
-        <button
-          className="text-[clamp(1.5rem,_1.7rem,_2rem)] text-start"
-          disabled={!user.initialized}
-          onClick={deleteAccount}
-        >
-          Delete Account
-        </button>
+          <button
+            className="text-[clamp(1.5rem,_1.7rem,_2rem)] text-start"
+            disabled={!user.initialized}
+            onClick={(e) => {
+              e.preventDefault();
+              // mark to show inputs
+            }}
+          >
+            Change Email
+          </button>
+          <button
+            className="text-[clamp(1.5rem,_1.7rem,_2rem)] text-start"
+            disabled={!user.initialized}
+            onClick={deleteAccount}
+          >
+            Delete Account
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
